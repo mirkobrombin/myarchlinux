95b1cfdb9bd9a7b605caa1d29012efb1a3c128eb
mirkobrombin <send@mirko.pm>
2020-03-27 14:52:58 +0100

    Openbox/Tint2 dotfiles and screenshot

---
+++ dotfiles/config/openbox/autostart.sh
@@ -0,0 +1,6 @@
+# ~/.config/openbox/autostart.sh
+xfsettingsd &
+xfce-mcs-manager &
+#(sleep 3 && /usr/bin/nm-applet --sm-disable) &
+~/.config/polybar/launch.sh
+feh --bg-scale Immagini/Wallpapers/rnfnibtbx1441.jpg &
